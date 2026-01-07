20447e21d658c47a38bac41d45b774d940a77131
rozhiadadashzadeh-ro <rojhia.dadashzadeh@gmail.com>
2026-01-07 22:05:13 +0330

    Add state management for all form fields

--- src/components/AddEventModal.jsx
+++ src/components/AddEventModal.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 
 export default function AddEventModal({ onClose, onAddEvent }) {
-
+    const [title, setTitle] = useState('');
+    const [datetime, setDatetime] = useState('');
+    const [description, setDescription] = useState('');
+    const [location, setLocation] = useState('');
+    const [notes, setNotes] = useState('');
 }
 
